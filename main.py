a36f6fa516766236ceccfe0841bc7992a06b0ec3
Crystiann <crystian_marica@yahoo.com>
2020-03-10 22:26:05 +0200

    first commit

---
+++ main.py
@@ -0,0 +1,43 @@
+from methods import *
+import secret
+
+
+# create an object from class Time()
+t = Time()
+browser = Browser()
+email = Email()
+computer = Computer()
+
+#  set the hours and minutes for start and stop scrip
+hour_start_time, minute_start_time = 7, 30
+hour_end_time, minute_end_time = 8, 25
+pointer_interval, wait_time_between_interval, wait_time_between_space = 4, 600, 1
+
+# Condition for starting script
+while t.time_now_hour() <= hour_start_time and t.time_now_minute() < minute_start_time:
+    t.time_wait(10)
+
+# Start time
+actual_time_start = t.time_now()
+start = t.start_time()
+# Send email start_scrip
+email.start_email(secret.emailFrom, secret.emailTo, secret.password, actual_time_start)
+# Open Browser
+browser.open_browser(secret.url, 10)
+
+# moving pointer and pause unpause so youtube doesnt stop
+computer.move_pointer(pointer_interval, wait_time_between_interval, wait_time_between_space)
+
+# Condition for closing script
+while t.time_now_hour() <= hour_end_time and t.time_now_minute() < minute_end_time:
+    t.time_wait(10)
+
+# Close Browser
+browser.close_browser(5)
+# End Time
+actual_time_end = t.time_now()
+end = t.format_time(t.start_time() - start)
+# Send email stop_script
+email.stop_email(secret.emailFrom, secret.emailTo, secret.password, actual_time_start, end, actual_time_end)
+# Shut down the computer
+computer.shut_down(5)
